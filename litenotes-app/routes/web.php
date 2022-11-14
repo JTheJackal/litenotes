895f10510b339e72e2aa573bc9bd687bc3006b11
Joshua Styles <styles@outlook.com>
2022-11-14 23:41:43 +0000

    Read notes from the note table

--- litenotes-app/routes/web.php
+++ litenotes-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('/notes', Notecontroller::class)->middleware(['auth']);
+
+require __DIR__.'/auth.php';
